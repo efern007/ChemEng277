Improved model analysis script so that we have normalization with StandardScaler. Additional bug fixes and improvements.

model_analysis.py
@@ -4,14 +4,16 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 """As a note for data analysis: all models will be trained and validated individually for each of the three 
 datasets: constant current, periodic, and synthetic. The real driving dataset will be used for testing purposes
 only. Normalized Discharge Capacity (or State of Health - "SOH") will be the target (y) variable for all models.
 Once random keys are determined for each dataset, those keys will be averaged to create one 2D list of parameters
 versus cycle number. The same method will be used for the validation set. The target variable matrix will be
-2D with SOH vs cycle number. These models will be fitted to Linear, Lasso, and Ridge Regression. Lasso and 
-Ridge Regression will be tested for various alpha (lambda) hyperparameters [0.1, 0.25, 0.5, 0.75, 0.9]. 
+2D with SOH vs cycle number. These models will be fitted to Linear, Lasso, and Ridge Regression. Lasso Regression 
+will be tested for various alpha (lambda) hyperparameters [0.1, 0.25, 0.5, 0.75, 0.9]. Ridge Regression will be
+tested for various alpha hyperparameters [1, 2.5, 5, 7.5, 9]. The Linear Regression model will be tested as is.
 For both the train and validation sets, the R^2 value will be calculated for each model. The model with the
 highest R^2 value will be selected to proceed to the test set. Because of potential variability in the train/validation
 split, a series of random combinations of train/validation sets will be tested. The model with the highest average
@@ -54,18 +56,39 @@ def inspect_all_data(data_constant_current, data_periodic, data_synthetic, data_
     inspect_dictionary(data_real_driving)
     return
 
-def create_train_val_sets(data):
+def create_train_val_sets(data, seed=77):
     # Save the headers of the DataFrames
     headers = data[list(data.keys())[0]].columns
+
+    np.random.seed(seed)
+
+    # randomly shuffle the data, split into train and validation sets, and average the train and validation sets
+    # add all the data.keys() into a numpy array and shuffle the array
+    data_keys = np.array(list(data.keys()))
+    np.random.shuffle(data_keys)
+
+    # add the first 80% of the shuffled array to the train set and the last 20% to the validation set
+    train_keys = data_keys[:int(0.8 * len(data_keys))]
+    val_keys = data_keys[int(0.8 * len(data_keys)):]
+
     # Create a dictionary to store the train and validation sets
     train_set = {}
     val_set = {}
-    for key in data.keys():
-        # Randomly shuffle the data
-        data[key] = data[key].sample(frac=1, random_state=1).reset_index(drop=True)
-        # Split the data into train and validation sets
-        train_set[key], val_set[key] = train_test_split(data[key], test_size=0.2, random_state=6)
+    # Add the data to the train and validation sets
+    for key in train_keys:
+        train_set[key] = data[key]
+    for key in val_keys:
+        val_set[key] = data[key]
+
+
+
+    # for key in data.keys():
+    #     # Randomly shuffle the data
+    #     data[key] = data[key].sample(frac=1, random_state=77).reset_index(drop=True)
+    #     # Split the data into train and validation sets
+    #     train_set[key], val_set[key] = train_test_split(data[key], test_size=0.2, random_state=6)
     
+
     # Determine the maximum number of rows across all DataFrames
     max_rows = max(df.shape[0] for df in data.values())
     
@@ -76,15 +99,21 @@ def create_train_val_sets(data):
     count_val_set = np.zeros((max_rows, 1))
     
     # Sum the train and validation sets and count the occurrences
-    for key in data.keys():
+    for key in train_set.keys():
         train_df = train_set[key].values
-        val_df = val_set[key].values
         for i in range(train_df.shape[0]):
             sum_train_set[i] += train_df[i]
             count_train_set[i] += 1
+
+    for key in val_set.keys():
+        val_df = val_set[key].values
         for i in range(val_df.shape[0]):
             sum_val_set[i] += val_df[i]
             count_val_set[i] += 1
+
+    # Create a dictionary to store the average train and validation sets
+    avg_train_set = {}
+    avg_val_set = {}
     
     # Compute the average, handling cases where the count is zero
     avg_train_set = np.divide(sum_train_set, count_train_set, out=np.zeros_like(sum_train_set), where=count_train_set!=0)
@@ -124,8 +153,13 @@ def create_train_val_sets(data):
 
 def create_models(train_set_X, val_set_X, train_set_y, val_set_y):
     """We will test linear regression, ridge regression, and lasso regression models.
-    Ridge and Lasso will be tested for various alpha values [0.1, 0.25, 0.5, 0.75, 0.9]
+    Lasso will be tested for various alpha values [0.1, 0.25, 0.5, 0.75, 0.9]
+    Ridge will be tested for various alpha values [1, 2.5, 5, 7.5, 9]
     Create a dictionary to store the R^2 values"""
+    scaler = StandardScaler()
+    train_set_X = scaler.fit_transform(train_set_X)
+    val_set_X = scaler.transform(val_set_X)
+
     r2_values = {}
     # Create a dictionary to store the models
     models = {}
@@ -140,20 +174,22 @@ def create_models(train_set_X, val_set_X, train_set_y, val_set_y):
             if model_name == "linear":
                 model = linear_model.LinearRegression()
             elif model_name == "ridge":
-                model = linear_model.Ridge(alpha=alpha)
+                model = linear_model.Ridge(alpha=10*alpha, max_iter=10000)
             elif model_name == "lasso":
-                model = linear_model.Lasso(alpha=alpha)
+                model = linear_model.Lasso(alpha=alpha, max_iter=10000)
             model.fit(train_set_X, train_set_y)
             r2 = model.score(val_set_X, val_set_y)
             r2_values[model_name].append(r2)
             models[model_name].append(model)
             if model_name == "linear":
                 model_names[model_name].append(model_name)
+                print(f"Model: {model_name}, alpha: {alpha}, R^2: {r2}")
+            elif model_name == "ridge":
+                model_names[model_name].append(model_name + f"_{10*alpha}")
+                print(f"Model: {model_name}, alpha: {10*alpha}, R^2: {r2}")
             else:
                 model_names[model_name].append(model_name + f"_{alpha}")
-
-            # Print the R^2 values
-            print(f"Model: {model_name}, alpha: {alpha}, R^2: {r2}")
+                print(f"Model: {model_name}, alpha: {alpha}, R^2: {r2}")
 
     # print all dictionaries
     print("R^2 Values:")
@@ -174,10 +210,10 @@ def create_models(train_set_X, val_set_X, train_set_y, val_set_y):
                 best_model = models[model_name][i]
                 best_model_name = model_names[model_name][i]
     print(f"Best Model: {best_model_name}")
-    return best_model, best_model_name, best_r2
+    return best_model, best_model_name, best_r2, scaler
 
 
-def test_model(best_model, best_model_name, data_real_driving):
+def test_model(best_model, best_model_name, data_real_driving, scaler):
     """Test the best model against all real driving data keys (8 in total)."""
     r2_values = {}
     # separate the target variable from the features for the real driving dataset
@@ -190,6 +226,8 @@ def test_model(best_model, best_model_name, data_real_driving):
         data_real_driving_X[key] = data_real_driving[key].drop(columns=["Normalized Discharge Capacity [-]"])
 
     for key in data_real_driving.keys():
+        # Apply the same scaling to the test data
+        data_real_driving_X[key] = scaler.transform(data_real_driving_X[key])
         r2 = best_model.score(data_real_driving_X[key], data_real_driving_y[key])
         r2_values[key] = r2
         print(f"Model: {best_model_name}, Key: {key}, R^2: {r2}")
@@ -215,8 +253,8 @@ def main():
         # Create train and validation sets
         train_set_X, val_set_X, train_set_y, val_set_y = create_train_val_sets(data)
         # create linear, lasso, and ridge regression models - save R^2 values to a table
-        best_model, best_model_name, best_r2_val = create_models(train_set_X, val_set_X, train_set_y, val_set_y)
-        avg_r2_test = test_model(best_model, best_model_name, data_real_driving)
+        best_model, best_model_name, best_r2_val, scaler = create_models(train_set_X, val_set_X, train_set_y, val_set_y)
+        avg_r2_test = test_model(best_model, best_model_name, data_real_driving, scaler)
         chosen_model[key] = best_model, best_model_name, best_r2_val, avg_r2_test
 
     print(chosen_model)